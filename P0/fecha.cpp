8a003a6485db946fa21bf136389a71343ede0aee
María Segura <114308383+mariasegura20@users.noreply.github.com>
2023-03-20 17:25:16 +0100

    P0 all tests passed

--- P0/fecha.cpp
+++ P0/fecha.cpp
@@ -8,45 +8,47 @@ using namespace std;
 // Constructor
 Fecha::Fecha(int d, int m, int a) : dia_(d), mes_(m), anno_(a)
 {
-    if (d == 0 || m == 0 || a == 0) {
-        std::time_t tiempo_calendario = std::time(nullptr);
-        std::tm* tiempo_descompuesto = std::localtime(&tiempo_calendario);
-        int dia = tiempo_descompuesto->tm_mday;
-        int mes = tiempo_descompuesto->tm_mon + 1; // tm_mon: 0 (ene)..11 (dic)
-        int anno = tiempo_descompuesto->tm_year + 1900; // tm_year: a~nos desde 1900
+	if (d == 0 || m == 0 || a == 0)
+	{
+		std::time_t tiempo_calendario = std::time(nullptr);
+		std::tm* tiempo_descompuesto = std::localtime(&tiempo_calendario);
+		int dia = tiempo_descompuesto->tm_mday;
+		int mes = tiempo_descompuesto->tm_mon + 1; // tm_mon: 0 (ene)..11 (dic)
+		int anno = tiempo_descompuesto->tm_year + 1900; // tm_year: a~nos desde 1900
 
-        if(dia_ == 0)
-            dia_ = dia;
-        if(mes_ == 0)
-            mes_ = mes;
-        if(anno_ == 0)
-            anno_ = anno;
-    }
-    
-    valida();
+		if(dia_ == 0)
+			dia_ = dia;
+		if(mes_ == 0)
+			mes_ = mes;
+		if(anno_ == 0)
+			anno_ = anno;
+	}
+
+	valida();
 }
 
 // Constructor de cadena de caracteres
 Fecha::Fecha(const char * s)
 {
-    int nuevoDia, nuevoMes, nuevoAnno;
-    int n = sscanf(s, "%d/%d/%d", &nuevoDia, &nuevoMes, &nuevoAnno);
-    if (n != 3)
-	throw Fecha::Invalida("ERROR! Formato incorrecto");
-    else {
-    	Fecha f(nuevoDia, nuevoMes, nuevoAnno);
-	dia_ = f.dia();
-	mes_ = f.mes();
-	anno_ = f.anno();
-    }
+	int nuevoDia, nuevoMes, nuevoAnno;
+	int n = sscanf(s, "%d/%d/%d", &nuevoDia, &nuevoMes, &nuevoAnno);
+	if (n != 3)
+		throw Fecha::Invalida("ERROR! Formato incorrecto");
+	else
+	{
+		Fecha f(nuevoDia, nuevoMes, nuevoAnno);
+		dia_ = f.dia();
+		mes_ = f.mes();
+		anno_ = f.anno();
+	}
 }
 
 // Operador de postincremento
 Fecha Fecha::operator++ (int) 
 {
-	Fecha t(*this);
+	Fecha t;
+	t = *this;
 	*this += 1;
-	valida();
 	return t;
 }
 
@@ -54,16 +56,15 @@ Fecha Fecha::operator++ (int)
 Fecha& Fecha::operator++ () 
 {
 	*this += 1;
-	valida();
 	return *this;
 }
 
 // Operador de postdecremento
 Fecha Fecha::operator-- (int) 
 {
-	Fecha t(*this);
+	Fecha t;
+	t = *this;
 	*this += -1;
-	valida();
 	return t;
 }
 
@@ -71,22 +72,23 @@ Fecha Fecha::operator-- (int)
 Fecha& Fecha::operator-- () 
 {
 	*this += -1;
-	valida();
 	return *this;
 }
 
-// Operador de suma (const)
+// Operador de suma
 Fecha Fecha::operator+ (int n) const
 {
-	Fecha t(*this);
+	Fecha t;
+	t = *this;
 	t += n;
 	return t;
 }
 
-// Operador de resta (const)
+// Operador de resta
 Fecha Fecha::operator- (int n) const
 {
-	Fecha t(*this);
+	Fecha t;
+	t = *this;
 	t += -n;
 	return t;
 }
@@ -94,28 +96,27 @@ Fecha Fecha::operator- (int n) const
 // Operador de suma en asignación
 Fecha& Fecha::operator+= (int n)
 {
-    struct tm when = {};
-    when.tm_mday = dia_;
-    when.tm_mon = mes_ - 1;
-    when.tm_year = anno_ - 1900;
-    when.tm_mday += n;
+	struct tm when = {};
+	when.tm_mday = dia_;
+	when.tm_mon = mes_ - 1;
+	when.tm_year = anno_ - 1900;
+	when.tm_mday += n;
 
-    mktime(&when);
+	mktime(&when);
 
-    this->dia_ = when.tm_mday;
-    this->mes_ = when.tm_mon + 1;
-    this->anno_ = when.tm_year + 1900;
+	this->dia_ = when.tm_mday;
+	this->mes_ = when.tm_mon + 1;
+	this->anno_ = when.tm_year + 1900;
 
-    valida();
-    return *this;
+	valida();
+	return *this;
 }
 
 // Operador de resta en asignación
 Fecha& Fecha::operator-= (int n)
 {
-    *this += -n;
-    valida();
-    return (*this);
+	*this += -n;
+	return *this;
 }
 
 // Operador de conversión
@@ -126,7 +127,9 @@ Fecha::operator const char*() const
 	f.tm_mday = dia_;
 	f.tm_mon = mes_ - 1;
 	f.tm_year = anno_ - 1900;
+	
 	mktime(&f);
+	
 	static char * fecha_str = new char [80];
 	strftime(fecha_str, 80, "%A %e de %B de %Y", &f);
 	return fecha_str;
@@ -135,30 +138,35 @@ Fecha::operator const char*() const
 // Métodos privados
 void Fecha::valida()
 {
-    if (anno_ < AnnoMinimo)
-        throw Invalida("ERROR! Desbordamiento inferior en la fecha");
-    else if (anno_ > AnnoMaximo)
-        throw Invalida("ERROR! Desbordamiento superior en la fecha");
-    
-    if (mes_ < 1 || mes_ > 12)
-        throw Invalida("ERROR! El mes no es válido");
-    
-    if (mes_ == 1 || mes_ == 3 || mes_ == 5 || mes_ == 7 || mes_ == 8 || mes_ == 10 || mes_ == 12) {
-        if (dia_ < 1 || dia_ > 31)
-            throw Invalida("ERROR! El día no es válido");
-    }
-    else if (mes_ == 4 || mes_ == 6 || mes_ == 9 || mes_ == 11) {
-        if (dia_ < 1 || dia_ > 30)
-            throw Invalida("ERROR! El día no es válido");
-    }
-    else if (mes_ == 2) {
-        if (anno_ % 4 == 0 && (anno_ % 400 == 0 || anno_ % 100 != 0)) {
-            if (dia_ < 1 || dia_ > 29)
-                throw Invalida("ERROR! El día no es válido");
-        }
-        else {
-            if (dia_ < 1 || dia_ > 28)
-                throw Invalida("ERROR! El día no es válido");
-        }
-    }
+	if (anno_ < AnnoMinimo)
+		throw Invalida("ERROR! Desbordamiento inferior en la fecha");
+	else if (anno_ > AnnoMaximo)
+		throw Invalida("ERROR! Desbordamiento superior en la fecha");
+
+	if (mes_ < 1 || mes_ > 12)
+		throw Invalida("ERROR! El mes no es válido");
+
+	if (mes_ == 1 || mes_ == 3 || mes_ == 5 || mes_ == 7 || mes_ == 8 || mes_ == 10 || mes_ == 12)
+	{
+		if (dia_ < 1 || dia_ > 31)
+			throw Invalida("ERROR! El día no es válido");
+	}
+	else if (mes_ == 4 || mes_ == 6 || mes_ == 9 || mes_ == 11)
+	{
+		if (dia_ < 1 || dia_ > 30)
+			throw Invalida("ERROR! El día no es válido");
+	}
+	else if (mes_ == 2)
+	{
+		if (anno_ % 4 == 0 && (anno_ % 400 == 0 || anno_ % 100 != 0))
+		{
+			if (dia_ < 1 || dia_ > 29)
+				throw Invalida("ERROR! El día no es válido");
+		}
+		else
+		{
+			if (dia_ < 1 || dia_ > 28)
+				throw Invalida("ERROR! El día no es válido");
+		}
+	}
 }
--- P0/fecha.hpp
+++ P0/fecha.hpp
@@ -5,52 +5,55 @@
 using namespace std;
 
 class Fecha {
-    public:
-    	static const int AnnoMinimo = 1902;
-        static const int AnnoMaximo = 2037;
-    
-        explicit Fecha(int d = 0, int m = 0, int a = 0);
-        Fecha(const char * s);
-        int dia() const noexcept;
-        int mes() const noexcept;
-        int anno() const noexcept;
-        Fecha operator++ (int);
-        Fecha& operator++ ();
-        Fecha operator-- (int);
-        Fecha& operator-- ();
-        Fecha operator+ (int n) const;
-        Fecha operator- (int n) const;
-        Fecha& operator += (int n);
-        Fecha& operator -= (int n);
-	operator const char*() const;
-        bool operator==(const Fecha& fecha) const;
-        bool operator!=(const Fecha& fecha) const;
-        bool operator<(const Fecha& fecha) const;
-        bool operator>(const Fecha& fecha) const;
-        bool operator>=(const Fecha& fecha) const;
-        bool operator<=(const Fecha& fecha) const;
-        
-        class Invalida {
-            public:
-                Invalida(const char * fallo):motivo(fallo){}
-                const char * por_que() const;
-            private:
-                const char * motivo;
-        };
+	public:
+		static const int AnnoMinimo = 1902;
+		static const int AnnoMaximo = 2037;    
 
-    private:
-        int dia_, mes_, anno_;
-        void valida();
+		explicit Fecha(int d = 0, int m = 0, int a = 0);
+		Fecha(const char * s);
+		int dia() const noexcept;
+		int mes() const noexcept;
+		int anno() const noexcept;
+		Fecha operator++ (int);
+		Fecha& operator++ ();
+		Fecha operator-- (int);
+		Fecha& operator-- ();
+		Fecha operator+ (int n) const;
+		Fecha operator- (int n) const;
+		Fecha& operator += (int n);
+		Fecha& operator -= (int n);
+		operator const char*() const;
+		bool operator==(const Fecha& fecha) const;
+		bool operator!=(const Fecha& fecha) const;
+		bool operator<(const Fecha& fecha) const;
+		bool operator>(const Fecha& fecha) const;
+		bool operator>=(const Fecha& fecha) const;
+		bool operator<=(const Fecha& fecha) const;
+		    
+		class Invalida {
+			public:
+				Invalida(const char * fallo):motivo(fallo){}
+				const char * por_que() const;
+			private:
+				const char * motivo;
+		};
+
+	private:
+		int dia_, mes_, anno_;
+		void valida();
 };
 
 // Observador del día
-inline int Fecha::dia() const noexcept { return dia_; }
+inline int Fecha::dia() const noexcept
+{ return dia_; }
 
 // Observador del mes
-inline int Fecha::mes() const noexcept { return mes_; }
+inline int Fecha::mes() const noexcept
+{ return mes_; }
 
 // Observador del año
-inline int Fecha::anno() const noexcept { return anno_; }
+inline int Fecha::anno() const noexcept
+{ return anno_; }
 
 // Operador de igualdad
 inline bool Fecha::operator==(const Fecha& fecha) const
