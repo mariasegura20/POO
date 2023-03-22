b2d473811968fab578d2ea600f7980fb160ee59a
María Segura <114308383+mariasegura20@users.noreply.github.com>
2023-03-22 10:23:48 +0100

    First version P1

---
+++ P1/cadena.cpp
@@ -0,0 +1,134 @@
+#include <iostream>
+#include <cstdio>
+#include <stdexcept>
+#include "cadena.hpp"
+
+using namespace std;
+
+/***** CONSTRUCTORES *****/
+// Constructor
+Cadena::Cadena(size_t t /* = 0*/, char c /* = ' '*/) : tam_(t), s_(new char[tam_+1])
+{
+    for (int i = 0 ; i < tam_ ; i++)
+        s_[i] = c;
+    s_[tam_] = '\0';
+};
+
+// Constructor de cadena de caracteres
+Cadena::Cadena(const char * c)
+{
+    tam_ = strlen(c);
+    s_ = new char[tam_+1];
+    strcpy(s_,c);
+    s_[tam_] = '\0';
+}
+
+// Constructor de movimiento
+Cadena::Cadena(Cadena&& cad) : tam_(cad.tam_), s_(cad.s_)
+{
+	cad.tam_ = 0;
+	delete[] cad.s_;
+	cad.s_ = new char[1];
+	s_[0] = '\0';
+}
+
+/***** OBSERVADORES *****/
+// Observador at (devuelve el caracter que ocupa la posición índice)
+char& Cadena::at (int indice)
+{
+    if (indice < tam_)
+        return s_[indice];
+    else
+        throw out_of_range("ERROR! Índice fuera de rango");
+}
+
+const char& Cadena::at (int indice) const
+{
+    if (indice < tam_)
+        return s_[indice];
+    else
+        throw out_of_range("ERROR! Índice fuera de rango");
+}
+
+//Observador subcadena
+Cadena Cadena::substr(int indice, int t) const
+{
+
+    if (indice + t <= tam_ && indice >= 0 && t >= 0) {
+    	char * s = new char[t+1];
+    	int i = 0;
+    	int fin = indice + t;
+    	while (indice < fin)
+    	{
+    	    s[i] = s_[indice];
+    	    i++;
+    	    indice++;
+    	}
+    	s[t] = '\0';
+    	Cadena Return(s);
+    	delete[] s;
+    	return Return;
+    }
+    else
+	throw out_of_range("ERROR! Índice fuera de rango");
+
+}
+
+/***** OPERADORES *****/
+// Operador de extracción
+istream& operator>>(istream& is, Cadena& cad)
+{
+	char * entrada = new char [32];
+	is >> entrada;
+	// Coger solo la palabra...
+	Cadena Aux(entrada);
+	cad = Aux;
+	delete[] entrada;
+	return is;
+}
+
+// Operador de inserción
+ostream& operator<<(ostream& os, const Cadena& cad)
+{
+    os << cad.substr(0, cad.length());
+    return os;
+}
+
+// Operador de asignación
+Cadena& Cadena::operator= (const Cadena& cad)
+{
+    if (*this != cad) {
+    	tam_ = cad.tam_;
+    	delete[] s_;
+    	s_ = new char[tam_+1];
+    	strcpy(s_, cad.s_);
+    }
+    return *this;
+}
+
+// Operador de suma en asignación
+Cadena& Cadena::operator+=(const Cadena& cad)
+{
+    if (tam_ != 0 && cad.tam_ != 0) {
+	char * concat = new char[tam_ + cad.tam_ + 1];
+	    
+	strcpy(concat, s_);
+	strcat(concat, cad.s_);
+	   
+	tam_ = tam_ + cad.tam_;
+	delete[] s_;
+	s_ = new char[tam_ + 1];
+	strcpy(s_, concat);
+	s_[tam_] = '\0';
+	delete[] concat;
+    }
+    return *this;
+}
+
+// Operador de suma
+const Cadena Cadena::operator+(const Cadena& cad) const
+{ 
+    Cadena copia(*this);
+    copia += cad;
+    return copia;
+}
---
+++ P1/cadena.hpp
@@ -0,0 +1,135 @@
+#ifndef CADENA_HPP
+#define CADENA_HPP
+
+#include <cstring>
+#include <iterator>
+
+using namespace std;
+
+class Cadena {
+    public:   
+        explicit Cadena(size_t t = 0, char c = ' ');
+        Cadena(const char * c);
+        Cadena(const Cadena& cad);
+        Cadena(Cadena&& cad);
+        
+        unsigned int length() const noexcept;
+        char& at (int indice);
+        const char& at (int indice) const;
+        Cadena substr(int indice, int t) const;
+        
+        // explicit operator const char*() const;
+		
+        Cadena& operator= (const Cadena& cad);
+        Cadena& operator+=(const Cadena& cad);
+        const Cadena operator+(const Cadena& cad1) const;
+        bool operator==(const Cadena& cad) const;
+        bool operator<(const Cadena& cad) const;
+		char& operator[](unsigned int indice); // lectura/escritura
+        char operator[](unsigned int indice) const; // lectura
+        
+        typedef char * iterator;
+		typedef const char * const_iterator;
+		typedef std::reverse_iterator<iterator> reverse_iterator;
+		typedef std::reverse_iterator<const_iterator> const_reverse_iterator;
+		iterator begin() const;
+		iterator end() const;
+		const_iterator cbegin() const;
+		const_iterator cend() const;
+		reverse_iterator rbegin() const;
+		reverse_iterator rend() const;
+		const_reverse_iterator crbegin() const;
+		const_reverse_iterator crend() const;
+        
+		~Cadena();
+		
+    private:
+        int tam_;
+        char * s_;
+};
+
+ostream& operator<<(ostream& os, const Cadena& cad);
+istream& operator>>(istream& is, Cadena& cad);
+
+bool operator!=(const Cadena& cad1, const Cadena& cad2);
+bool operator>(const Cadena& cad1, const Cadena& cad2);
+bool operator<=(const Cadena& cad1, const Cadena& cad2);
+bool operator>=(const Cadena& cad1, const Cadena& cad2);
+bool operator==(const char* s, const Cadena& cad);
+
+/***** CONSTRUCTORES *****/
+// Constructor de copia
+inline Cadena::Cadena(const Cadena& cad) : tam_(cad.tam_), s_(new char[tam_+1])
+{ strcpy(s_, cad.s_); }
+
+/***** OBSERVADORES *****/
+// Observador de tamaño
+inline unsigned int Cadena::length() const noexcept{ return(tam_); }
+
+// Operador de conversión
+// inline Cadena::operator const char*() const
+// { return s_; }
+
+/***** OPERADORES *****/
+// Operador de igualdad
+inline bool Cadena::operator==(const Cadena& cad) const
+{ return(strcmp(s_, cad.s_) == 0); }
+
+inline bool operator==(const char* s, const Cadena& cad)
+{ return Cadena(s) == cad; }
+
+// Operador de desigualdad
+inline bool operator!=(const Cadena& cad1, const Cadena& cad2)
+{ return (!(cad1 == cad2)); }
+
+// Operador de comparación menor que
+inline bool Cadena::operator<(const Cadena& cad) const
+{ return(strcmp(s_, cad.s_) < 0); }
+
+// Operador de comparación mayor que
+inline bool operator>(const Cadena& cad1, const Cadena& cad2)
+{ return (!(cad1 < cad2) && !(cad1 == cad2)); }
+
+// Operador de comparación menor o igual que
+inline bool operator<=(const Cadena& cad1, const Cadena& cad2)
+{ return ((cad1 < cad2) || (cad1 == cad2)); }
+
+// Operador de comparación mayor o igual que
+inline bool operator>=(const Cadena& cad1, const Cadena& cad2)
+{ return (!(cad1 < cad2)); }
+
+// Operador [] 
+inline char& Cadena::operator[](unsigned int indice) { return(s_[indice]); }
+
+inline char Cadena::operator[](unsigned int indice) const { return(s_[indice]); }
+
+/***** ITERADORES *****/
+inline Cadena::iterator Cadena::begin() const
+{ return s_; }
+
+inline Cadena::iterator Cadena::end() const
+{ return s_ + length(); }
+
+inline Cadena::const_iterator Cadena::cbegin() const
+{ return s_; }
+
+inline Cadena::const_iterator Cadena::cend() const
+{ return s_ + length();}
+
+inline Cadena::reverse_iterator Cadena::rbegin() const
+{ return reverse_iterator(end()); }
+
+inline Cadena::reverse_iterator Cadena::rend() const
+{ return reverse_iterator(begin()); }
+
+inline Cadena::const_reverse_iterator Cadena::crbegin() const
+{ return const_reverse_iterator(end()); }
+
+inline Cadena::const_reverse_iterator Cadena::crend() const
+{ return const_reverse_iterator(begin()); }
+
+/***** DESTRUCTOR *****/
+inline Cadena::~Cadena()
+{ delete[] s_; }
+
+#endif
